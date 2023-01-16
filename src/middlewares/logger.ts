f554b356c0914bdc3f89c06cf6632b9f3b7447ee
Anup Joseph <arunjoseph3007@gmail.com>
2023-01-16 11:30:03 +0530

    Added readme

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,7 +2,14 @@ import { handlerFunction } from "../interfaces/handler";
 
 export const logger: handlerFunction = (req, _, next) => {
   console.log(
-    `[${new Date().toTimeString().slice(0, 8)}] ${req.method} ${req.url}`
+    "\x1b[0m",
+    "\x1b[36m",
+    `[${new Date().toTimeString().slice(0, 8)}]`,
+    "\x1b[33m",
+    req.method,
+    "\x1b[32m",
+    req.url,
+    "\x1b[0m"
   );
   next();
 };
